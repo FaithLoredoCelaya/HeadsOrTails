29599ba24c09fa9d21c20ff76acc4421a4d0e9e9
Faith Loredo <Faithc.2555@gmail.com>
2021-06-22 12:03:13 -0400

    Add project files.

---
+++ HeadsOrTails/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HeadsOrTails
+{
+    partial class HeadsOrTails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HeadsOrTails));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Roll = new System.Windows.Forms.Button();
+            this.outBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.outBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Heads Or Tails!";
+            // 
+            // Roll
+            // 
+            this.Roll.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Roll.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Roll.Location = new System.Drawing.Point(170, 74);
+            this.Roll.Name = "Roll";
+            this.Roll.Size = new System.Drawing.Size(75, 23);
+            this.Roll.TabIndex = 1;
+            this.Roll.Text = "Go!";
+            this.Roll.UseVisualStyleBackColor = false;
+            this.Roll.Click += new System.EventHandler(this.Roll_Click);
+            // 
+            // outBox
+            // 
+            this.outBox.Location = new System.Drawing.Point(13, 109);
+            this.outBox.Name = "outBox";
+            this.outBox.Size = new System.Drawing.Size(389, 440);
+            this.outBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.outBox.TabIndex = 2;
+            this.outBox.TabStop = false;
+            // 
+            // HeadsOrTails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(414, 561);
+            this.Controls.Add(this.outBox);
+            this.Controls.Add(this.Roll);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "HeadsOrTails";
+            this.Text = "Heads Or Tails";
+            ((System.ComponentModel.ISupportInitialize)(this.outBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Roll;
+        private System.Windows.Forms.PictureBox outBox;
+    }
+}
---
+++ HeadsOrTails/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadsOrTails
+{
+    public partial class HeadsOrTails : Form
+    {
+        public HeadsOrTails()
+        {
+            InitializeComponent();
+        }
+
+        private void Roll_Click(object sender, EventArgs e)
+        {
+            //Initialize random processing.
+            Random rng = new Random();
+            int output = rng.Next(2);
+
+
+            //check the result and change image.
+            if (output == 1)
+                outBox.Image = Properties.Resources.Heads;
+            else if (output == 0)
+                outBox.Image = Properties.Resources.Tails;
+        }
+    }
+}
